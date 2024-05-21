ebb55dd210a1ce3d9786a7b95a79a0ed28cf7b70
Kiran70586 <164142467+Kiran70586@users.noreply.github.com>
2024-05-21 11:03:43 +0530

    Add files via upload

---
+++ Cart Table.sql
@@ -0,0 +1,9 @@
+
+* Sql Query
+
+CREATE TABLE cart (
+cart_id INT AUTO_INCREMENT PRIMARY KEY,
+product_id INT NOT NULL,
+quantity INT NOT NULL DEFAULT 1,
+FOREIGN KEY (product_id) REFERENCES products(id));
+
